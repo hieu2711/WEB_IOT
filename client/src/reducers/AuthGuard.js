a7ddffeb42625035312274cbd92516814843e1fd
Hieu <2051052042hieu@ou.edu.vn>
2024-05-17 22:55:01 +0700

    fix login

--- client/src/reducers/AuthGuard.js
+++ client/src/reducers/AuthGuard.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from './userSlice';
+import { Navigate } from 'react-router-dom';
 
 function AuthGuard({ children }) {
     const isLoggedIn = useSelector(selectIsLoggedIn);
